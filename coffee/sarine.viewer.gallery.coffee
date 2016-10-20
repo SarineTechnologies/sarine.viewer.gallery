1dd9b386a30780aa01c786a56dd1246ced70fa68
adica <carmeladi@gmail.com>
2016-10-20 10:44:41 +0300

    gallery plugin

--- coffee/sarine.viewer.gallery.coffee
+++ coffee/sarine.viewer.gallery.coffee
@@ -1,31 +1,92 @@
 ###!
-sarine.viewer.gallery - v0.4.0 -  Tuesday, October 18th, 2016, 3:59:14 PM 
+sarine.viewer.gallery - v0.4.0 -  Thursday, October 20th, 2016, 10:33:11 AM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
 ###
 class SarineGallery extends Viewer
 
 	constructor: (options) -> 			
 		super(options) 
-		{@ImagesPath, @ImageName, @ImageExtention} = options				
+		{@ImagesPath, @ImagesArr, @ImageExtention} = options
+		@isAvailble = true
+		@resourcesPrefix = options.baseUrl + "atomic/v1/assets/"	
+		@resources = [
+	      {element:'script',src:'jquery.bxslider.min.js'},
+	      {element:'link',src:'jquery.bxslider.css'}
+	    ]					
 
 	convertElement : () ->	
-		@canvas = $("<canvas>")		
-		@ctx = @canvas[0].getContext('2d')
-		@element.append(@canvas)
-	        
+		@ul = $("<ul class='bxslider'>")
+		@div = $("<div id='bx-pager'>")
+		@element.append(@div)
+		@element.append(@ul)
+		
+	preloadAssets: (callback)=>
+
+	    loaded = 0
+	    totalScripts = @resources.map (elm)-> elm.element =='script'
+	    triggerCallback = (callback) ->
+	      loaded++
+	      if(loaded == totalScripts.length-1 && callback!=undefined )
+	        setTimeout( ()=> 
+	          callback() 
+	        ,500) 
+
+	    element
+	    for resource in @resources
+	      element = document.createElement(resource.element)
+	      if(resource.element == 'script')
+	        $(document.body).append(element)
+	        element.onload = element.onreadystatechange = ()-> triggerCallback(callback)
+	        element.src = @resourcesPrefix + resource.src + cacheVersion
+	        element.type= "text/javascript"
+
+	      else
+	        element.href = @resourcesPrefix + resource.src + cacheVersion
+	        element.rel= "stylesheet"
+	        element.type= "text/css"
+	        $(document.head).prepend(element) 
+
 	first_init : ()->
 		defer = $.Deferred() 
+
+		@element.append()
 		_t = @
-		src = configuration.configUrl + @ImagesPath + @ImageName + @ImageExtention
-		_t.loadImage(src).then((img)->
-			_t.canvas.attr {'width':img.width, 'height': img.height}
-			_t.ctx.drawImage img , 0 , 0 			
-			defer.resolve(_t) 				
-		)  
+		@preloadAssets ()->
+			src = configuration.configUrl + _t.ImagesPath + _t.ImagesArr[0]  + _t.ImageExtention
+			_t.loadImage(src).then((img)->
+				if img.src.indexOf('data:image') == -1 && img.src.indexOf('no_stone') == -1			
+					defer.resolve(_t)
+				else
+					_t.isAvailble = false
+					_t.element.empty()
+					@canvas = $("<canvas>")		
+					@canvas[0].width = img.width
+					@canvas[0].height = img.height
+					@ctx = @canvas[0].getContext('2d')
+					@ctx.drawImage(img, 0, 0, img.width, img.height)
+					@canvas.attr {'class' : 'no_stone'}					
+					_t.element.append(@canvas)
+			)
 		defer
 
 	full_init : ()-> 
 		defer = $.Deferred() 
+		if @isAvailble
+			@imageList = @element.find('.bxslider')
+			@thumbList = @element.find('#bx-pager')
+			for name, index in @ImagesArr
+				@fullSrc = configuration.configUrl + @ImagesPath + name + @ImageExtention
+				@thumbSrc = configuration.configUrl + @ImagesPath + name + '-thumb' + @ImageExtention
+				@imageItem = '<li><img src="' + @fullSrc + '" alt="' + name + '" /></li>'
+				@thumbItem = '<a data-slide-index="'+ index + '" href=""><img src="' + @thumbSrc + '" alt="' + name + '-thumb" /></a>'
+				@imageList.append(@imageItem)
+				@thumbList.append(@thumbItem)
+			
+			$('.bxslider').bxSlider({
+				pagerCustom: '#bx-pager',
+				controls: false
+			}); 
+		
 		defer.resolve(@)		
 		defer
 		
--- dist/sarine.viewer.gallery.js
+++ dist/sarine.viewer.gallery.js
@@ -1,11 +1,12 @@
 
 /*!
-sarine.viewer.gallery - v0.4.0 -  Tuesday, October 18th, 2016, 3:59:14 PM 
+sarine.viewer.gallery - v0.4.0 -  Thursday, October 20th, 2016, 10:33:11 AM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
 (function() {
   var SarineGallery,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
@@ -13,35 +14,118 @@ sarine.viewer.gallery - v0.4.0 -  Tuesday, October 18th, 2016, 3:59:14 PM
     __extends(SarineGallery, _super);
 
     function SarineGallery(options) {
+      this.preloadAssets = __bind(this.preloadAssets, this);
       SarineGallery.__super__.constructor.call(this, options);
-      this.ImagesPath = options.ImagesPath, this.ImageName = options.ImageName, this.ImageExtention = options.ImageExtention;
+      this.ImagesPath = options.ImagesPath, this.ImagesArr = options.ImagesArr, this.ImageExtention = options.ImageExtention;
+      this.isAvailble = true;
+      this.resourcesPrefix = options.baseUrl + "atomic/v1/assets/";
+      this.resources = [
+        {
+          element: 'script',
+          src: 'jquery.bxslider.min.js'
+        }, {
+          element: 'link',
+          src: 'jquery.bxslider.css'
+        }
+      ];
     }
 
     SarineGallery.prototype.convertElement = function() {
-      this.canvas = $("<canvas>");
-      this.ctx = this.canvas[0].getContext('2d');
-      return this.element.append(this.canvas);
+      this.ul = $("<ul class='bxslider'>");
+      this.div = $("<div id='bx-pager'>");
+      this.element.append(this.div);
+      return this.element.append(this.ul);
+    };
+
+    SarineGallery.prototype.preloadAssets = function(callback) {
+      var element, loaded, resource, totalScripts, triggerCallback, _i, _len, _ref, _results;
+      loaded = 0;
+      totalScripts = this.resources.map(function(elm) {
+        return elm.element === 'script';
+      });
+      triggerCallback = function(callback) {
+        loaded++;
+        if (loaded === totalScripts.length - 1 && callback !== void 0) {
+          return setTimeout((function(_this) {
+            return function() {
+              return callback();
+            };
+          })(this), 500);
+        }
+      };
+      element;
+      _ref = this.resources;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        resource = _ref[_i];
+        element = document.createElement(resource.element);
+        if (resource.element === 'script') {
+          $(document.body).append(element);
+          element.onload = element.onreadystatechange = function() {
+            return triggerCallback(callback);
+          };
+          element.src = this.resourcesPrefix + resource.src + cacheVersion;
+          _results.push(element.type = "text/javascript");
+        } else {
+          element.href = this.resourcesPrefix + resource.src + cacheVersion;
+          element.rel = "stylesheet";
+          element.type = "text/css";
+          _results.push($(document.head).prepend(element));
+        }
+      }
+      return _results;
     };
 
     SarineGallery.prototype.first_init = function() {
-      var defer, src, _t;
+      var defer, _t;
       defer = $.Deferred();
+      this.element.append();
       _t = this;
-      src = configuration.configUrl + this.ImagesPath + this.ImageName + this.ImageExtention;
-      _t.loadImage(src).then(function(img) {
-        _t.canvas.attr({
-          'width': img.width,
-          'height': img.height
+      this.preloadAssets(function() {
+        var src;
+        src = configuration.configUrl + _t.ImagesPath + _t.ImagesArr[0] + _t.ImageExtention;
+        return _t.loadImage(src).then(function(img) {
+          if (img.src.indexOf('data:image') === -1 && img.src.indexOf('no_stone') === -1) {
+            return defer.resolve(_t);
+          } else {
+            _t.isAvailble = false;
+            _t.element.empty();
+            this.canvas = $("<canvas>");
+            this.canvas[0].width = img.width;
+            this.canvas[0].height = img.height;
+            this.ctx = this.canvas[0].getContext('2d');
+            this.ctx.drawImage(img, 0, 0, img.width, img.height);
+            this.canvas.attr({
+              'class': 'no_stone'
+            });
+            return _t.element.append(this.canvas);
+          }
         });
-        _t.ctx.drawImage(img, 0, 0);
-        return defer.resolve(_t);
       });
       return defer;
     };
 
     SarineGallery.prototype.full_init = function() {
-      var defer;
+      var defer, index, name, _i, _len, _ref;
       defer = $.Deferred();
+      if (this.isAvailble) {
+        this.imageList = this.element.find('.bxslider');
+        this.thumbList = this.element.find('#bx-pager');
+        _ref = this.ImagesArr;
+        for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
+          name = _ref[index];
+          this.fullSrc = configuration.configUrl + this.ImagesPath + name + this.ImageExtention;
+          this.thumbSrc = configuration.configUrl + this.ImagesPath + name + '-thumb' + this.ImageExtention;
+          this.imageItem = '<li><img src="' + this.fullSrc + '" alt="' + name + '" /></li>';
+          this.thumbItem = '<a data-slide-index="' + index + '" href=""><img src="' + this.thumbSrc + '" alt="' + name + '-thumb" /></a>';
+          this.imageList.append(this.imageItem);
+          this.thumbList.append(this.thumbItem);
+        }
+        $('.bxslider').bxSlider({
+          pagerCustom: '#bx-pager',
+          controls: false
+        });
+      }
       defer.resolve(this);
       return defer;
     };
